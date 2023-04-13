d4849f12419b1e1760d6169b4af8eda789f49285
HRD <HRD@DESKTOP-MIFHSTI>
2023-04-13 17:19:12 +0900

    mvc 예제

---
+++ src/main/java/com/jjcompany/mvc/HomeController.java
@@ -0,0 +1,68 @@
+package com.jjcompany.mvc;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.annotation.ModelAndViewResolver;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+@RequestMapping(value="/gallery")
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	@RequestMapping(value = "/image")
+	public String image() {
+		return "image";
+	}
+	@RequestMapping(value = "/board/list")
+	public String list() {
+		return "board/list"; //폴더아래의 jsp 파일 열기
+	}
+	@RequestMapping(value = "content")
+	public String content(Model model) {
+		
+		model.addAttribute("content", "안녕하세요");
+		
+		return "content"; 
+	}
+	@RequestMapping(value = "mvtest")
+	public ModelAndView mvtest() {
+		
+		ModelAndView mv =new ModelAndView();
+		mv.addObject("content","안녕");
+		mv.setViewName("mvtest");
+		
+		
+		
+		return mv; 
+	}
+	
+}
